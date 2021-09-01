f037499273cbb56ca89907cc8737c8663e007ea8
boatwrong <86577724+boatwrong@users.noreply.github.com>
2021-09-01 16:09:47 -0500

    IT WORKS

--- Test.java
+++ Test.java
@@ -43,18 +43,21 @@ public class Test {
 		 */
 		for(int i=0; i<h-1; i++) // iterate through modifier rows
 		{
-			for(int j=1; j<h-1; j++) // iterate through rows to be modified
+			for(int j=i+1; j<h; j++) // iterate through rows to be modified
 			{
 				double m = (arr[j][i] / arr[i][i]);
 				
 				for(int k=i; k<h; k++) // iterate through columns to modify elements
 				{
-					arrCopy[i][k] = m * arr[i][k]; // multiply modifying row by m
-
+					// arrCopy[i][k] = m * arr[i][k];
+					this.arr[j][k] = this.arr[j][k] - m * this.arr[i][k];
+					
+					/*
 					for(int g = 0; g<h; g++) // subtract modified term from row to be modified
 					{
-						arr[j][g] = arr[j][g] - arrCopy[j][g];
+						arr[j][k] = arr[j][k] - arrCopy[i][k];
 					}
+					*/
 				}
 				
 				/*
@@ -67,7 +70,7 @@ public class Test {
 				*/
 			}
 		}
-		return arr;
+		return this.arr;
 	}
 	
 	public void printArr()
